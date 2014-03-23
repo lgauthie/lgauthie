5800dc450c1e09bc009e115811e069f962633334
Lee Gauthier <lgauthie@uvic.ca>
2014-03-23 02:13:07 -0700

    Add start of blog stuffs

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -18,9 +18,51 @@ getHomeR = do
     --     handlerName = "getHomeR" :: Text
     defaultLayout $ do
         aDomId <- newIdent
+        setTitle "Welcome!"
+        $(widgetFile "homepage")
+        $(fayFile' (ConE 'StaticR) "Home")
+
+-- The view showing the list of articles
+getBlogR :: Handler Html
+getBlogR = do
+    -- Get the list of articles inside the database.
+    articles <- runDB $ selectList [] [Desc ArticleTitle]
+    -- We'll need the two "objects": articleWidget and enctype
+    -- to construct the form (see templates/articles.hamlet).
+    (articleWidget, enctype) <- generateFormPost entryForm
+    defaultLayout $ do
+        setTitle "Welcome To Yesod!"
+        $(widgetFile "new_entry")
+
+getArticleR :: ArticleId -> Handler Html
+getArticleR articleId = do
+    article <- runDB $ get404 articleId
+    defaultLayout $ do
+        setTitle $ toHtml $ articleTitle article
+        $(widgetFile "article")
+
+entryForm :: Form (FileInfo, Text)
+entryForm = renderDivs $ (,)
+    <$> fileAFormReq "Choose a file"
+    <*> areq textField "What's on the file?" Nothing
+-- entryForm :: Form Article
+-- entryForm = renderDivs $ Article
+--     <$> areq   textField "Title" Nothing
+--     <*> areq   nicHtmlField "Content" Nothing
+
+getProjR :: Handler Html
+getProjR = do
+    -- (formWidget, formEnctype) <- generateFormPost sampleForm
+    -- let submission = Nothing :: Maybe (FileInfo, Text)
+    --     handlerName = "getHomeR" :: Text
+    defaultLayout $ do
+        aDomId <- newIdent
         setTitle "Welcome To Yesod!"
         $(widgetFile "homepage")
         $(fayFile' (ConE 'StaticR) "Home")
+
+postBlogR :: Handler Html
+postBlogR = undefined
 
 -- postHomeR :: Handler Html
 -- postHomeR = do
